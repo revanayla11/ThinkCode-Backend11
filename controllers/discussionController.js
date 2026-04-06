783a1155336835276591a53876a19e85301675a3
Reva Nayla <revanano11@gmail.com>
2026-04-06 23:06:20 +0700

    workspace

--- controllers/discussionController.js
+++ controllers/discussionController.js
@@ -1032,7 +1032,7 @@ END`;
 exports.startRoomTimer = async (req, res) => {
   try {
     const { roomId } = req.params;
-    const duration = 3600 * 1000; // 60 minutes
+    const duration = 5400 * 1000; 
     const timerEnd = new Date(Date.now() + duration);
     
     await DiscussionRoom.update(
@@ -1057,7 +1057,7 @@ exports.checkTimerPenalty = async (req, res) => {
     const room = await DiscussionRoom.findByPk(roomId);
     
     if (!room.timerEnd) {
-      return res.json({ timeLeft: 3600, overtimeMinutes: 0, penaltyXP: 0 });
+      return res.json({ timeLeft: 5400, overtimeMinutes: 0, penaltyXP: 0 });
     }
     
     const now = Date.now();
