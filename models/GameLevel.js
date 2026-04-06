b23521a7febda1db36d54dcded8019acb8c9286f
Reva Nayla <revanano11@gmail.com>
2026-04-07 01:07:33 +0700

    workspace11

--- models/GameLevel.js
+++ models/GameLevel.js
@@ -1,41 +1,32 @@
-const { Model } = require("sequelize");
-
 module.exports = (sequelize, DataTypes) => {
-  class GameLevel extends Model {
-    static associate(models) {
-      GameLevel.belongsTo(models.Materi, { foreignKey: "materi_id" });
-      GameLevel.belongsTo(models.Badge, { foreignKey: "reward_badge_id" });
-      GameLevel.hasMany(models.GameQuestion, { foreignKey: "levelId" });
-    }
-  }
-
-  GameLevel.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      materi_id: DataTypes.INTEGER,
-      levelNumber: DataTypes.INTEGER,
-      title: DataTypes.STRING,
-      type: {
-        type: DataTypes.ENUM("quiz", "flashcard", "memory", "typing", "sort"),
-        defaultValue: "quiz",
-      },
-      totalQuestions: DataTypes.INTEGER,
-      reward_xp: DataTypes.INTEGER,
-      reward_badge_id: DataTypes.INTEGER,
-      isActive: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-      },
+  const GameLevel = sequelize.define("GameLevel", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-      sequelize,
-      modelName: "GameLevel",
-    }
-  );
+    materi_id: DataTypes.INTEGER,
+    levelNumber: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    type: {
+      type: DataTypes.ENUM("quiz", "flashcard", "memory", "typing", "sort"),
+      defaultValue: "quiz",
+    },
+    totalQuestions: DataTypes.INTEGER,
+    reward_xp: DataTypes.INTEGER,
+    reward_badge_id: DataTypes.INTEGER,
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  });
+
+  // ✅ RELASI
+  GameLevel.associate = (models) => {
+    GameLevel.belongsTo(models.Materi, { foreignKey: "materi_id" });
+    GameLevel.belongsTo(models.Badge, { foreignKey: "reward_badge_id" });
+    GameLevel.hasMany(models.GameQuestion, { foreignKey: "levelId" });
+  };
 
   return GameLevel;
 };
--- models/GameQuestion.js
+++ models/GameQuestion.js
@@ -1,45 +1,36 @@
-const { Model } = require("sequelize");
-
 module.exports = (sequelize, DataTypes) => {
-  class GameQuestion extends Model {
-    static associate(models) {
-      GameQuestion.belongsTo(models.GameLevel, {
-        foreignKey: "levelId",
-      });
-    }
-  }
-
-  GameQuestion.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      levelId: DataTypes.INTEGER,
-      content: DataTypes.TEXT,
-      type: {
-        type: DataTypes.ENUM(
-          "mcq",
-          "essay",
-          "truefalse",
-          "dragdrop",
-          "typing",
-          "sort"
-        ),
-        defaultValue: "mcq",
-      },
-      meta: DataTypes.JSON,
-      points: {
-        type: DataTypes.INTEGER,
-        defaultValue: 10,
-      },
+  const GameQuestion = sequelize.define("GameQuestion", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-      sequelize,
-      modelName: "GameQuestion",
-    }
-  );
+    levelId: DataTypes.INTEGER,
+    content: DataTypes.TEXT,
+    type: {
+      type: DataTypes.ENUM(
+        "mcq",
+        "essay",
+        "truefalse",
+        "dragdrop",
+        "typing",
+        "sort"
+      ),
+      defaultValue: "mcq",
+    },
+    meta: DataTypes.JSON,
+    points: {
+      type: DataTypes.INTEGER,
+      defaultValue: 10,
+    },
+  });
+
+  // ✅ RELASI
+  GameQuestion.associate = (models) => {
+    GameQuestion.belongsTo(models.GameLevel, {
+      foreignKey: "levelId",
+    });
+  };
 
   return GameQuestion;
 };
