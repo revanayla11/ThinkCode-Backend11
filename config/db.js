8d1aa292cb14f68d077bd4f5c98a5ce5bd89f3e0
Reva Nayla <revanano11@gmail.com>
2026-04-06 23:39:24 +0700

    workspace1

--- config/db.js
+++ config/db.js
@@ -1,15 +1,22 @@
+require("dotenv").config(); // ⬅️ WAJIB di atas
+
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.MYSQL_URL, {
-  dialect: "mysql",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+console.log("MYSQL_URL:", process.env.MYSQL_URL); // debug
+
+const sequelize = new Sequelize(
+  process.env.MYSQL_URL || "mysql://root:@127.0.0.1:3306/thinkcode", // fallback
+  {
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-});
+  }
+);
 
 sequelize.authenticate()
   .then(() => console.log("✅ Database connected."))
