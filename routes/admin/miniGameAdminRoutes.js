35fe54a3b76619c3ec42c77d158abb1e4f8f7a7e
Reva Nayla <revanano11@gmail.com>
2026-04-07 23:19:38 +0700

    minigame

--- routes/admin/miniGameAdminRoutes.js
+++ routes/admin/miniGameAdminRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const verifyToken = require("../../middleware/verifyToken");
 const isAdmin = require("../../middleware/isAdmin");
-const ctrl = require("../../controllers/adminGameController");
+const ctrl = require("../../controllers/miniGameAdminController");
 
 router.get("/materi/:materiId/game-levels", verifyToken, isAdmin, ctrl.getMateriGameLevels);
 router.get("/game/level/:levelId/questions", verifyToken, isAdmin, ctrl.getLevelQuestions);
