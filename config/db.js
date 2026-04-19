ebd02a8c1029b7a24a903911b03ec75800675272
Reva Nayla <revanano11@gmail.com>
2026-04-19 12:52:38 +0700

    deploy hostinger

--- config/db.js
+++ config/db.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 3306,
     dialect: "mysql",
     logging: false,
